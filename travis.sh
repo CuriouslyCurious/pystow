3fb7765453d068d7aa00ae7fec5f2d0f5e5fdea7
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2018-03-18 23:08:01 +0100

    Add python3 infront of stow calls

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 git clone https://github.com/CuriouslyCurious/dotfiles.git
 stow.py
-./stow.py | yes
-./stow.py | yes
-./stow.py -R | yes
-./stow.py -r | yes n
+python3 stow.py | yes
+python3 stow.py | yes
+python3 stow.py -R | yes
+python3 stow.py -r | yes n
