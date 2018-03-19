546d8d46b8f27d3d80daf22b8e4a5e5a07d8c8bd
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2018-03-19 14:28:35 +0100

    Attempt at copying dotfiles repo to home folder

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
-git clone https://github.com/CuriouslyCurious/dotfiles.git
+git clone https://github.com/CuriouslyCurious/dotfiles.git $HOME/dotfiles
 stow.py
 yes | python3 stow.py 
 yes | python3 stow.py 
