2fdf7a1283c4bfea3f84fd6501821d363b9b439c
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2018-03-19 14:47:00 +0100

    Add some more test outputs

--- travis.sh
+++ travis.sh
@@ -1,8 +1,39 @@
 #/usr/bin/env bash
 git clone https://github.com/CuriouslyCurious/dotfiles.git $HOME/dotfiles
-stow.py
+
+echo "=========================="
+echo "First run"
+echo "=========================="
 yes | python3 stow.py 
+ls -la $HOME/.config
+
+echo "=========================="
+echo "Ensure it doesn't stow when something exists."
+echo "=========================="
 yes | python3 stow.py 
+ls -la $HOME/.config
+
+echo "=========================="
+echo "Check if replacing stuff works"
+echo "=========================="
 yes | python3 stow.py -R
+ls -la $HOME/.config
+
+echo "=========================="
+echo "Remove everything"
+echo "=========================="
 yes | python3 stow.py -r
-yes n | python3 stow.py
+ls -la $HOME/.config
+
+echo "=========================="
+echo "Try to run again and see if anything remained"
+echo "=========================="
+yes | python3 stow.py
+ls -la $HOME/.config
+
+echo "=========================="
+echo "Remove everything again."
+echo "=========================="
+yes | python3 stow.py
+ls -la $HOME
+ls -la $HOME/.config
