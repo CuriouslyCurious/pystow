99ff5d022f2376616e1f61687f4075b96817845a
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2018-03-19 14:22:02 +0100

    Correct pipe placement in travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,8 @@
 #/usr/bin/env bash
 git clone https://github.com/CuriouslyCurious/dotfiles.git
 stow.py
-python3 stow.py | yes
-python3 stow.py | yes
-python3 stow.py -R | yes
-python3 stow.py -r | yes n
+yes | python3 stow.py 
+yes | python3 stow.py 
+yes | python3 stow.py -R
+yes | python3 stow.py -r
+yes n | python3 stow.py
